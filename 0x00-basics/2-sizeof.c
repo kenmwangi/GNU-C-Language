854cc52dd0bc7337f41504bc107e2388ae8f1e75
Ken Mwangi <kenmwangi071@gmail.com>
2022-09-14 15:54:21 +0300

    sizes of each data type

---
+++ 0x00-basics/2-sizeof.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*
+ * print: size of data types
+ * return 0: (success)
+ */
+int main(void)
+{
+	int i;
+	double d;
+	char c;
+
+	printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));
+	printf("The size of an double is: %lu.\n", (unsigned long)sizeof(d));
+	printf("The size of an c is: %lu.\n", (unsigned long)sizeof(c));
+
+	return (0);
+
+}
